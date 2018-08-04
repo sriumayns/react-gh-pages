51f82df6ea39ffcb1f5006df2ee0ddc30bafda0b
Mayu <mayuuzu@gmail.com>
2018-08-04 22:08:45 +0700

    Test route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import {Test} from './components/Test';
+import {Hello} from './components/Hello';
+
 class App extends Component {
   render() {
     return (
@@ -12,6 +16,16 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        <Router>
+          <div>
+            <Link to='/test'>Test</Link><br/>
+            <Link to='/hello'>Hello</Link>
+            <Switch>
+              <Route exact path='/test' component={Test} />
+              <Route exact path='/hello' component={Hello} />
+            </Switch>
+          </div>
+        </Router>
         </p>
       </div>
     );
